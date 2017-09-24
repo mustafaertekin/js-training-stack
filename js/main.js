ef5ee8ba36d7302626b77533422edb131e32dc21
Mehmet Dogan <info@intelligentworks.ch>
2017-09-24 22:05:51 +0200

    change peek

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@
  * You find the program logic, data structures and function calls in this file 
  */
 
+"use strict";
+
 
 let stack = new Array();
 
@@ -11,7 +13,7 @@ stack.add(4);
 stack.add(-2);
 stack.add(11);
 
-stack.ಠ_ಠ(); // 11
+stack.peek(); // 11
 
 stack.remove();  // 11
 stack.remove();  // -2
